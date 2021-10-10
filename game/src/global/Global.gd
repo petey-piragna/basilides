c9de0f9236511d66524aef39bdca5e09d2a392a2
Aberdeen Beatty <nlbeatty42@gmail.com>
2021-10-10 14:58:40 -0400

    add level generation

--- game/src/global/Global.gd
+++ game/src/global/Global.gd
@@ -1,6 +1,8 @@
 extends Node
 
 onready var heaven_stack = []
+onready var level_heaven = []
+onready var heaven_available = [1,2,4,8]
 
 func set_heavens(model):
 	print("hi!!!")
--- game/src/main/LevelSelect.gd
+++ game/src/main/LevelSelect.gd
@@ -4,6 +4,7 @@ onready var HeavenCount = $"BaseScreen/HeavenCount"
 onready var Global = get_tree().get_root().get_node("Global")
 onready var heavens_remaining = Global.get_heavens()
 
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -14,7 +15,34 @@ func _ready():
 	HeavenCount.text = str(heavens_remaining.size())
 	pass # Replace with function body.
 
+func spend_heavens(level):
+	print("charging for level " + str(level))
+	var i = 0
+	while heavens_remaining.size() > 0:
+		if heavens_remaining[i][0] % Global.heaven_available[level - 1] == 0 && heavens_remaining[i][0] != 0:
+			print(heavens_remaining[i])
+			Global.level_heaven = heavens_remaining[i].slice(1, heavens_remaining[i].size())
+			heavens_remaining.pop_front()
+			break
+		else:
+			heavens_remaining.pop_front()
+	HeavenCount.text = str(heavens_remaining.size())
+	if heavens_remaining.size() == 0:
+		print("all out")
+	else:
+		get_tree().change_scene("res://src/level/Level" + str(level) + ".tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+
+func _on_LevelOne_pressed():
+	spend_heavens(1)
+
+
+func _on_LevelTwo_pressed():
+	spend_heavens(2)
+
+func _on_LevelThree_pressed():
+	spend_heavens(3)
+
+func _on_LevelFour_pressed():
+	spend_heavens(4)
