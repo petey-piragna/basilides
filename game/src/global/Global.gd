e1f2f0dfe3f7c372e2073279e49c7a6eb31c49ca
Aberdeen Beatty <nlbeatty42@gmail.com>
2021-10-10 15:26:41 -0400

    add layer update based on level heaven values

--- game/src/global/Global.gd
+++ game/src/global/Global.gd
@@ -3,6 +3,7 @@ extends Node
 onready var heaven_stack = []
 onready var level_heaven = []
 onready var heaven_available = [1,2,4,8]
+onready var filter_colors = [1.5, 1.5, 1.5]
 
 func set_heavens(model):
 	print("hi!!!")
--- game/src/level/LevelBase.gd
+++ game/src/level/LevelBase.gd
@@ -2,17 +2,32 @@ extends Node2D
 onready var player = $Player
 onready var stage = 1
 onready var camera = $Camera2D
+onready var Global = $"/root/Global"
+onready var level_heaven = Global.level_heaven
+onready var heaven_filter = $HeavenFilter
+onready var filter_colors = Global.filter_colors
+onready var temp_color = [150, 150, 150, 100]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_update_filter()
 	pass # Replace with function body.
+
+func _update_filter():
+	var i = 0
+	while i < 3:
+		temp_color[i] = level_heaven[i] * filter_colors[i] 
+		i += 1
+	temp_color[3] = 255
+	heaven_filter.color = Color8(temp_color[0], temp_color[1], temp_color[2], temp_color[3])
+	print(heaven_filter.color)
 
 func progress():
 	camera.position.x += 320
 	player.position.x = 15 + stage * 320
 	stage += 1
-	if stage == 10:
+	if stage == 2:
 			end()
 
 func end():
